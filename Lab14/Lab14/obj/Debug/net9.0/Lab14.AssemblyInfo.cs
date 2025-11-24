883b1b2d17bff3945129c0a287ee482089998ac4
Ebad Chua Quispe <ebad.chua@tecup.edu.pe>
2025-11-23 20:20:23 -0500

    Mover Dockerfile a raíz y corregir configuración para Render

--- Lab14/Lab14/obj/Debug/net9.0/Lab14.AssemblyInfo.cs
+++ Lab14/Lab14/obj/Debug/net9.0/Lab14.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3b24af62be1af1d8ad50efb070c8a08a7a5e4b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
